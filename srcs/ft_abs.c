4184c463bd76d0c262c0c8140a3196430a49beef
Raspurrin <vondetou@gmail.com>
2021-11-30 22:29:33 +0100

    Added ft_abs and new version of ft_itoa.

--- srcs/ft_abs.c
+++ srcs/ft_abs.c
@@ -1,38 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*   ft_abs.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/15 21:10:48 by mialbert          #+#    #+#             */
-/*   Updated: 2021/11/30 22:27:25 by mialbert         ###   ########.fr       */
+/*   Created: 2021/11/30 00:59:09 by mialbert          #+#    #+#             */
+/*   Updated: 2021/11/30 13:39:37 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(t_int32 nbr)
+t_int32	ft_abs(t_int32 x)
 {
-	char	*out;
-	t_size	i;
-
-	i = ft_intlen(nbr);
-	if (nbr == -2147483648)
-		return (ft_strdup("-2147483648"));
-	out = malloc(i + 1 * sizeof(char));
-	if (nbr == 0)
-		out[0] = '0';
-	else if (nbr < 0)
-	{
-		out[0] = '-';
-		nbr = ft_abs(nbr);
-	}
-	out[i--] = '\0';
-	while (nbr > 0)
-	{
-		out[i--] = nbr % 10 + '0';
-		nbr /= 10;
-	}
-	return (out);
+	if (x < 0)
+		return (x *= -1);
+	return (x);
 }
