f20d090a67d81c4f5e2a7f982447d38e4081187d
Raspurrin <vondetou@gmail.com>
2021-12-02 23:47:24 +0100

    Bonus fix yet again.

--- srcs/ft_lstnew_bonus.c
+++ srcs/ft_lstnew_bonus.c
@@ -6,13 +6,13 @@
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 21:12:10 by mialbert          #+#    #+#             */
-/*   Updated: 2021/12/02 01:06:40 by mialbert         ###   ########.fr       */
+/*   Updated: 2021/12/02 23:45:43 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstnew(void *content)
+t_list	*ft_lstnew_bonus(void *content)
 {
 	t_list	*node;
 
