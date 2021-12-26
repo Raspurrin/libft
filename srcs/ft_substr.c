0afe4a0ba0c412221d83356c8ce8d16886d9a6f9
Raspurrin <vondetou@gmail.com>
2021-12-26 19:56:38 +0100

    Removed start > len.

--- srcs/ft_substr.c
+++ srcs/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 21:20:13 by mialbert          #+#    #+#             */
-/*   Updated: 2021/12/02 00:48:54 by mialbert         ###   ########.fr       */
+/*   Updated: 2021/12/24 14:19:00 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_substr(char const *str, t_uint32 start, t_size len)
 	slen = ft_strlen(str);
 	if (!str)
 		return (NULL);
-	if (start > len || start > slen)
+	if (start >= slen)
 		return (ft_strdup(""));
 	if (len > slen - start)
 		return (ft_strdup(str + start));
@@ -31,3 +31,26 @@ char	*ft_substr(char const *str, t_uint32 start, t_size len)
 	out[len] = '\0';
 	return (out);
 }
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	char	dst[10];
+// 	static char	dst2[10];
+// 	char	src[] = "hgiello";
+// 	char	src2[] = "lolk";
+// 	printf("before: %s\n", dst);
+// 	ft_substr(dst, src, 8);
+// 	printf("after: %s\n", dst);
+// 	// printf("before: %s\n", dst2);
+// 	// ft_strlcpy(dst2, src, 8);
+// 	// printf("after: %s\n", dst2);
+// 	// printf("before: %s\n", dst2);
+// 	// ft_strlcpy(dst2, src + 2, 8);
+// 	// printf("after: %s\n", dst2);
+// 	// printf("before: %s\n", dst2);
+// 	// ft_strlcpy(dst2, dst2 + 2, 8);
+// 	// printf("after: %s\n", dst2);
+// 	return (0);
+// }
