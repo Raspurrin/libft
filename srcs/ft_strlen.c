e6202f0d4d09a7e6622de553f92586e34bace0ab
Raspurrin <vondetou@gmail.com>
2021-12-29 18:44:57 +0100

    Changed from gnl to ft.

--- srcs/ft_strlen.c
+++ srcs/ft_strlen.c
@@ -6,11 +6,11 @@
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 21:19:34 by mialbert          #+#    #+#             */
-/*   Updated: 2021/12/27 20:25:46 by mialbert         ###   ########.fr       */
+/*   Updated: 2021/12/29 00:42:21 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	gnl_ft_strlen(const char *str)
+int	ft_strlen(const char *str)
 {
 	int	i;
 
