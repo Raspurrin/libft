5e540ce78de601f2509bfa357880212f7ba4b707
Raspurrin <vondetou@gmail.com>
2022-11-16 20:00:56 +0100

    really weird

--- srcs/ft_realloc.c
+++ srcs/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 21:18:38 by mialbert          #+#    #+#             */
-/*   Updated: 2022/11/16 15:04:22 by mialbert         ###   ########.fr       */
+/*   Updated: 2022/11/16 20:00:29 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,18 @@ void	*ft_realloc(void *ptr, size_t size)
 {
 	size_t	i;
 	char	*out;
-	uint8_t	*ptr2;
 
 	i = 0;
-	ptr2 = (uint8_t *)ptr; 
 	if (!ptr)
 		return (NULL);
 	out = malloc(size);
 	if (!out)
 		return (NULL);
-	while (ptr2[i])
+	while (size-- > 0)
 	{
-		out[i] = ptr2[i];
+		out[i] = ((uint8_t *)ptr)[i];
 		i++;
 	}
+	// free(ptr);
 	return (out);
 }
