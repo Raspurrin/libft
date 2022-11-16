127c8c257d605a55d9559b407ef0c373bee12da7
Raspurrin <vondetou@gmail.com>
2022-11-16 15:31:01 +0100

    Don't free a local pointer lol -realloc

--- srcs/ft_realloc.c
+++ srcs/ft_realloc.c
@@ -6,28 +6,30 @@
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 21:18:38 by mialbert          #+#    #+#             */
-/*   Updated: 2022/11/04 01:04:59 by mialbert         ###   ########.fr       */
+/*   Updated: 2022/11/16 15:04:22 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
+#include <stdio.h>
 
 void	*ft_realloc(void *ptr, size_t size)
 {
 	size_t	i;
 	char	*out;
+	uint8_t	*ptr2;
 
 	i = 0;
+	ptr2 = (uint8_t *)ptr; 
 	if (!ptr)
 		return (NULL);
 	out = malloc(size);
 	if (!out)
 		return (NULL);
-	while (size-- > 0)
+	while (ptr2[i])
 	{
-		out[i] = ((uint8_t *)ptr)[i];
+		out[i] = ptr2[i];
 		i++;
 	}
-	free(ptr);
 	return (out);
 }
