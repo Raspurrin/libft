a6c98642c1a4b8986749f47806bec00d673a0d61
Djaisin <vondetou@gmail.com>
2022-11-26 05:29:57 +0100

    Deleted tests bc I don't use them and they annoying

--- tests/test_ft_pow.c
+++
@@ -1,8 +0,0 @@
-#include "../srcs/libft.h"
-
-int	test_ft_pow(void)
-{
-	printf("5 by the power of 5 is: %ld\n", ft_pow(5, 2));
-	printf("10 by the power of 3 is: %ld\n", ft_pow(10, 3));
-	return (0);
-}
--- tests/test_ft_strjoin.c
+++
@@ -1,9 +0,0 @@
-#include "../srcs/libft.h"
-
-int	test_ft_strjoin(void)
-{
-	char	str1[] = "I have ";
-	char	str2[] = "a cookie";
-	printf("Your function: %s\n", ft_strjoin(str1, str2));
-	return (0);
-}
